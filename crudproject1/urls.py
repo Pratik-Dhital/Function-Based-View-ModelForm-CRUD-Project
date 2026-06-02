9f6a0d446563d4ec6e8ebdd31bb1db66caca8035
Pratik-Dhital <pratikdhital2060@gmail.com>
2026-06-02 16:50:06 +0545

    performed add and view operation and also made the templates for them

--- crudproject1/urls.py
+++ crudproject1/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from enroll.views import add_and_show, update
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add-and-show/',add_and_show, name="add-and-show" ),
+    path('update/', update, name="update"),
 ]
